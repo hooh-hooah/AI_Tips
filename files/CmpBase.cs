573af61ea366a09fad7ebb0b09c3d227c4297e0c
hooh <55322444+hooh-hooah@users.noreply.github.com>
2019-12-01 09:17:11 +0900

    Added CmpBase/CmpHair.cs
    used for hair modding

---
+++ files/CmpBase.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace AIChara {
+    public abstract class CmpBase : MonoBehaviour {
+        private bool baseDB;
+
+        public CmpBase(bool _baseDB) {
+            this.baseDB = _baseDB;
+        }
+
+        public abstract void SetReferenceObject();
+
+        private DynamicBone[] dynamicBones;
+
+        [Header("カメラ内判定用")]
+        public Renderer[] rendCheckVisible;
+
+        public int reacquire;
+    }
+}
